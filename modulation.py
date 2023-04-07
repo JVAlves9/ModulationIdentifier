ba50dd58c4555f24d14f9a08bcc8e032cb846563
João Victor Alves <d-jvictor@hotmail.com>
2023-04-06 23:20:53 -0300

    Add simulation and range simulation

--- modulation.py
+++ modulation.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pyphysim.modulators import fundamental
 from pyphysim.util.conversion import dB2Linear
-from pyphysim.util.misc import pretty_time, randn_c
+from pyphysim.util.misc import randn_c
 
-class Data:
+class DataTransmitionSimulator():
     """Class used to generate all data to transmit, as well as  the modulation objects
     """    
     def __init__(self, num_symbols : int, num_symbols_transmit : int) -> None:
@@ -16,20 +16,19 @@ class Data:
         """             
         self.num_symbols = num_symbols
         self.num_symbols_transmit = num_symbols_transmit
-        self.data = np.random.randint(0, self.num_symbols, self.num_symbols_transmit)
         self.__psk = fundamental.PSK(self.num_symbols)
         self.__qam = fundamental.QAM(self.num_symbols)
-        self.__psk_modulated_data = None
-        self.__qam_modulated_data = None
-        self.__psk_demodulated_data = None
-        self.__qam_demodulated_data = None
 
+    def generate_data(self) -> np.ndarray :
+        return np.random.randint(0, self.num_symbols, self.num_symbols_transmit)
 
-    def modulate_data(self) -> None:
-        """Generate the modulated data for PSK and qam
+    def modulate_data(self, data) -> tuple:
+        """Generate the modulated data for PSK and QAM
         """        
-        self.__psk_modulated_data = self.__psk.modulate(self.data)
-        self.__qam_modulated_data = self.__qam.modulate(self.data)
+        psk_modulated_data = self.__psk.modulate(data)
+        qam_modulated_data = self.__qam.modulate(data)
+
+        return (qam_modulated_data, psk_modulated_data)
 
     def __awgn_noise(self, noise : int):
         noise_power = 1 / dB2Linear(noise)
@@ -40,41 +39,118 @@ class Data:
         return np.exp(1j * np.random.randn(self.num_symbols_transmit))
     
     def print_constellations(self):
+        print(self.__qam.symbols.real)
         plt.plot(self.__qam.symbols.real, self.__qam.symbols.imag, '.', label="QAM")
         plt.plot(self.__psk.symbols.real, self.__psk.symbols.imag, '.', label="PSK")
         plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1),
           fancybox=True, shadow=True, ncol=5)
         plt.grid(True)
-        
         plt.show()
     
-    def transmit_data(self, noise : int = 20):
+    def transmit_data(self, qam_modulated_data, psk_modulated_data, noise : int = 20) -> tuple:
         """Transmit the generated data through a channel with AWGN
 
         Args:
             noise (int, optional): The noise to be applied to the data in dB. Defaults to 20dB.
-        """        
+
+        Returns:
+            tuple: (QAM ndarray data with noise, PSK ndarray with noise)
+        """    
         channel_awg_noise = self.__awgn_noise(noise)
-        noisy_qam = self.__qam_modulated_data + channel_awg_noise
-        noisy_psk = self.__psk_modulated_data + channel_awg_noise
+        noisy_qam = qam_modulated_data + channel_awg_noise
+        noisy_psk = psk_modulated_data + channel_awg_noise
 
-        self.__qam_demodulated_data = self.__qam.demodulate(noisy_qam)
-        self.__psk_demodulated_data = self.__psk.demodulate(noisy_psk)
+        return (noisy_qam, noisy_psk)
+    
+    def demodulate(self, qam_data : np.ndarray, psk_data : np.ndarray):
+        qam_demodulated_data = self.__qam.demodulate(qam_data)
+        psk_demodulated_data = self.__psk.demodulate(psk_data)
+
+        return (qam_demodulated_data, psk_demodulated_data)
 
 
+    def symbol_error_rate(self, data, qam_demodulated_data, psk_demodulated_data):
+        return (1 - sum(qam_demodulated_data == data) / self.num_symbols_transmit, 1 - sum(psk_demodulated_data == data) / self.num_symbols_transmit)
+        # qam_error = sum(qam_demodulated_data != data)
+        # psk_error = sum(psk_demodulated_data != data)
 
-        # figure, axis = plt.subplots(1, 2)
+        # qam_simulate_results = SimulationResults()
+        # psk_simulate_results = SimulationResults()
+
+        # qam_simulate_results.add_new_result("symbol_error_rate", Result.RATIOTYPE, values=qam_error, total=self.num_symbols_transmit)
+        # psk_simulate_results.add_new_result("symbol_error_rate", Result.RATIOTYPE, values=psk_error, total=self.num_symbols_transmit)
+        # return (qam_simulate_results, psk_simulate_results)
+    
+    def simulate(self, num_rep : int = 5000, noise = 20):
+        qam_ser = 0
+        psk_ser = 0
+
+        for rep in range(num_rep):
+            data = self.generate_data()
+            qam_mod_data, psk_mod_data = self.modulate_data(data)
+            qam_data, psk_data = self.transmit_data(qam_mod_data, psk_mod_data, noise)
+            qam_demo, psk_demo = self.demodulate(qam_data, psk_data)
+            qam_result, psk_result = self.symbol_error_rate(data, qam_demo, psk_demo)
+            qam_ser += qam_result
+            psk_ser += psk_result
+
+        return (qam_ser/num_rep, psk_ser/num_rep)
+    
+    def simulate_range_noise(self, initial_noise, final_noise, num_rep : int = 5000):
+        qam_ser_values = []
+        psk_ser_values = []
+
+        for noise in range(initial_noise, final_noise):
+            qam_ser, psk_ser = self.simulate(num_rep, noise)
+            qam_ser_values.append(qam_ser)
+            psk_ser_values.append(psk_ser)
         
-        # axis[0].plot(self.__qam_modulated_data.real, self.__qam_modulated_data.imag, '.')
-        # axis[0].grid(True)
+        return (qam_ser_values, psk_ser_values)
+    
 
-        # axis[1].plot(np.real(noisy_qam),np.imag(noisy_qam), '.')
-        # axis[1].grid(True)
-        # plt.show()
+    def ser_teoretical_value(self, noise : int = 20):
+        return (self.__qam.calcTheoreticalSER(noise),self.__psk.calcTheoreticalSER(noise))
+    
+    def ser_teoretical_value_noise_range(self, initial_noise, final_noise):
+        qam_ser_values = []
+        psk_ser_values = []
+        for noise in range(initial_noise, final_noise):
+            qam_ser, psk_ser = self.ser_teoretical_value(noise)
+            qam_ser_values.append(qam_ser)
+            psk_ser_values.append(psk_ser)
+        
+        return (qam_ser_values,psk_ser_values)
+    
+    def ser_plot(self, qam_ser, qam_ser_teoretical, psk_ser, psk_ser_teoretical, inital_noise, final_noise):
+        noise_list = [noise for noise in range(inital_noise, final_noise)]
+
+        figure, axis = plt.subplots(1, 2)
+        axis[0].plot(noise_list, qam_ser, label="QAM")
+        axis[0].plot(noise_list, qam_ser_teoretical, '.', label="QAM Teoretical")
+        axis[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.1),
+          fancybox=True, shadow=True, ncol=5)
+        axis[0].grid(True)
+
+        axis[1].plot(noise_list, psk_ser, label="PSK")
+        axis[1].plot(noise_list, psk_ser_teoretical, '.', label="PSK Teoretical")
+        axis[1].legend(loc='upper center', bbox_to_anchor=(0.5, 1.1),
+          fancybox=True, shadow=True, ncol=5)
+        axis[1].grid(True)
+        plt.show()
+
 
 if __name__ == "__main__":
-    cla = Data(64,32)
-    cla.modulate_data()
-    cla.transmit_data()
+    cla = DataTransmitionSimulator(64,64)
+    print(f"Simulated SER value : {cla.simulate()}\nTeoretical SER valeu : {cla.ser_teoretical_value()}")
+    qam_ser, psk_ser = cla.simulate_range_noise(-5,15,2000)
+    qam_ser_ter, psk_ser_ter = cla.ser_teoretical_value_noise_range(-5,15)
+    cla.ser_plot(
+        qam_ser,
+        qam_ser_ter,
+        psk_ser,
+        psk_ser_ter,
+        -5,
+        15
+    )
     # cla.print_constellations()
     
