dd6109fded41ac1c36da2f13055a2218dcb98d1f
João Victor Alves <d-jvictor@hotmail.com>
2023-04-09 16:31:52 -0300

    Intermediary commit

--- modulation_identifier.py
+++ modulation_identifier.py
@@ -1,1 +1,12 @@
 import pywt
+
+from modulation import DataTransmitionSimulator as Data
+
+def main():
+    transmition = Data(64, int(1e3))
+    data = transmition.generate_data()
+    qam_modulated, psk_modulated = transmition.modulate_data(data)
+
+    pywt.cwt()
+
+print(pywt.wavelist())
